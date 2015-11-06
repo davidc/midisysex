d0dd37795fef31597eb6725424305f39e95dd624
davidc <david@emfcamp.org>
2015-11-06 02:12:37 +0100

    add license

--- src/net/davidc/midisysex/MidiSysEx.java
+++ src/net/davidc/midisysex/MidiSysEx.java
@@ -9,7 +9,30 @@ import java.util.Scanner;
 
 
 /**
- * Created by david on 05/11/2015.
+ * Tool to send and monitor SysEx messages from MIDI devices.
+ * <p/>
+ * Copyright (c) 2015 David C A Croft.
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author David Croft (<a href="http://www.davidc.net/" title="http://www.davidc.net/">http://www.davidc.net/</a>)
+ * @version $Id$
  */
 public class MidiSysEx implements ActionListener
 {
